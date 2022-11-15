415c7ddb495e609ad4df024c5f8fba7995f2e44e
douar <mouadouarzong@gmail.com>
2022-11-15 15:32:52 -0600

    Unsub from sub to surveyList

--- src/app/components/survey-list/survey-list.component.ts
+++ src/app/components/survey-list/survey-list.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {SurveyService} from "../../services/survey.service";
 import {ISurvey} from "../../_Interfaces/ISurvey";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-survey-list',
   templateUrl: './survey-list.component.html',
   styleUrls: ['./survey-list.component.css']
 })
-export class SurveyListComponent implements OnInit {
+export class SurveyListComponent implements OnInit, OnDestroy {
 
   surveyList: ISurvey[] = []
+  surveyListSub: Subscription
 
   constructor(private surveyService: SurveyService) {
-    this.surveyService.$surveyList.subscribe(
+    this.surveyListSub = this.surveyService.$surveyList.subscribe(
       surveyList => {this.surveyList = surveyList}
     )
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.surveyListSub.unsubscribe();
   }
 
 }

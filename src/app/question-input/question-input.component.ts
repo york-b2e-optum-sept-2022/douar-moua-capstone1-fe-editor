5ae32bc2e134d0a159f221e33acbbe8a1a42cdfb
douar <mouadouarzong@gmail.com>
2022-11-17 13:33:07 -0600

    Reconfigure id creation for create new survey success ng g c question-input

---
+++ src/app/question-input/question-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-input',
+  templateUrl: './question-input.component.html',
+  styleUrls: ['./question-input.component.css']
+})
+export class QuestionInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
